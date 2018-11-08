bc28fab9d6b0ed3eefed8c6f6f9c1e368681f46f
Eugene Kwon <ekwon86@gmail.com>
2018-11-08 09:40:42 -0800

    adding userform

---
+++ src/components/UserForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+impot FormUserDetails from './FormUserDetails';
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to prev step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle field change
+  handlechange = input => e => {
+    this.setState({
+      [input]: e.target.value
+    });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handlechange={this.handlechange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
+  }
+}
+
+export default UserForm;
